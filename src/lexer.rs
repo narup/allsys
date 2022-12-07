c6c4fdff93ebbf99f1e976b94ad3ccd881779827
Puran SS <mailtopuran@gmail.com>
2022-12-07 00:20:46 -0800

    parse function

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,12 +1,20 @@
 #![allow(dead_code)]
 
-struct Token {
+static ILLEGAL: &'static str = "ILLEGAL";
+static PLUS: &'static str = "+";
+static MINUS: &'static str = "-";
+static LPAREN: &'static str = "(";
+static RPAREN: &'static str = "(";
+
+#[derive(Debug)]
+pub struct Token {
     token_type: TokenType,
-    val: String,
+    val: &'static str,
+    col: usize,
 }
 
-#[derive(strum_macros::Display)]
-enum TokenType {
+#[derive(strum_macros::Display, Debug)]
+pub enum TokenType {
     ILLEGAL,
     EOF,
 
@@ -34,31 +42,88 @@ enum TokenType {
 
 pub struct Lexer {
     input: String,
-    position: u16,      // current position in input (points to current char)
-    read_position: u16, // current reading position in input, after current char
-    ch: char,
+    position: usize,      // current position in input (points to current char)
+    read_position: usize, // current reading position in input, after current char
+    ch: u8,
 }
 
 impl Lexer {
-    pub fn next_token(&self) -> String {
-        String::from(&self.input)
+    pub fn parse(&mut self) -> Vec<Token> {
+        let tokens = Vec::new();
+
+        while self.position < 4 {
+            self.read_char();
+            self.increment_read_position();
+        }
+
+        tokens
+    }
+
+    pub fn next_token(&self) -> Token {
+        let bytes = &self.input.as_bytes();
+
+        println!("Loop over bytes");
+        for (i, &item) in bytes.iter().enumerate() {
+            match item {
+                b'+' => return get_token(i, TokenType::PLUS, PLUS),
+                _ => return get_token(i, TokenType::ILLEGAL, ILLEGAL),
+            }
+        }
+        get_token(0, TokenType::ILLEGAL, ILLEGAL)
+    }
+
+    fn read_char(&self) -> Option<char> {
+        let r = self.input.chars().nth(self.position);
+        println!("Value of r: {:?}", r);
+        r
+    }
+
+    fn increment_read_position(&mut self) {
+        self.position = self.position + 1;
+    }
+}
+
+fn get_token(col: usize, token_type: TokenType, val: &'static str) -> Token {
+    Token {
+        col,
+        token_type,
+        val,
     }
 }
 
 pub fn new(input: String) -> Lexer {
     Lexer {
-        input,
+        input: input.to_string(),
         position: 0,
         read_position: 0,
-        ch: '\0',
+        ch: b' ',
     }
 }
 
-pub fn print_tokens() {
-    let t1 = Token {
-        token_type: TokenType::ILLEGAL,
-        val: String::from("ILLEGAL"),
-    };
+pub fn mod_name() -> String {
+    "lexer".to_string()
+}
 
-    println!("First token: ({}, {})", t1.token_type.to_string(), t1.val);
+//----- Tests ---
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::mod_name;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!("lexer", mod_name());
+    }
+
+    #[test]
+    fn lexer_test() {
+        let input = String::from("+(-)");
+
+        let mut l = new(input);
+        l.parse();
+
+        println!("test passed")
+    }
 }
